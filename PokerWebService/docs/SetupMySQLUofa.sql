3461a1143c54fdd6f6dc7f4f92cdafdc715f5a29
Dustin Durand <dddurand@ualberta.ca>
2013-03-05 00:03:38 -0700

    Updating Documentation.

--- PokerWebService/docs/SetupMySQLUofa.sql
+++ PokerWebService/docs/SetupMySQLUofa.sql
@@ -2,6 +2,11 @@ delimiter $$
 
 USE `ece493grp2`$$
 
+DROP TABLE IF EXISTS `game_actions`$$
+DROP TABLE IF EXISTS `games`$$
+DROP TABLE IF EXISTS `misc_data`$$
+DROP TABLE IF EXISTS `user_table`$$
+
 delimiter $$
 
 CREATE TABLE `game_actions` (
@@ -15,7 +20,7 @@ CREATE TABLE `game_actions` (
   `pot` int(11) NOT NULL DEFAULT '0',
   `bet` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`,`accountID`,`gameID`)
-) ENGINE=InnoDB AUTO_INCREMENT=142 DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=184 DEFAULT CHARSET=utf8$$
 
 
 delimiter $$
@@ -26,7 +31,7 @@ CREATE TABLE `games` (
   `date_uploaded` date NOT NULL,
   PRIMARY KEY (`id`,`gameUUID`),
   UNIQUE KEY `gameUUID_UNIQUE` (`gameUUID`)
-) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8$$
 
 
 delimiter $$
@@ -38,7 +43,7 @@ CREATE TABLE `misc_data` (
   `value` blob NOT NULL,
   `date_uploaded` date NOT NULL,
   PRIMARY KEY (`id`,`accountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=152 DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=164 DEFAULT CHARSET=utf8$$
 
 
 delimiter $$
@@ -53,14 +58,14 @@ CREATE TABLE `user_table` (
   `optimality_WEEK` double DEFAULT '0',
   `optimality_MONTH` double DEFAULT '0',
   `optimality_YEAR` double DEFAULT '0',
-  `delta_money_ALL` int(11) DEFAULT '0',
-  `delta_money_DAY` int(11) DEFAULT '0',
-  `delta_money_WEEK` int(11) DEFAULT '0',
-  `delta_money_MONTH` int(11) DEFAULT '0',
-  `delta_money_YEAR` int(11) DEFAULT '0',
+  `net_money_ALL` int(11) DEFAULT '0',
+  `net_money_DAY` int(11) DEFAULT '0',
+  `net_money_WEEK` int(11) DEFAULT '0',
+  `net_money_MONTH` int(11) DEFAULT '0',
+  `net_money_YEAR` int(11) DEFAULT '0',
   PRIMARY KEY (`id`,`username`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `auth_token_UNIQUE` (`auth_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8$$
 
 
