ea9e4fab69dc46bf54720f4f813b9d27ed710245
Dustin Durand <dddurand@ualberta.ca>
2013-03-16 15:13:31 -0600

    Attaching base database functionality to login

---
+++ Android/src/dataModels/MoneyGenerated.java
@@ -0,0 +1,56 @@
+package dataModels;
+
+public class MoneyGenerated {
+
+	private final String name = "MoneyGenerated";
+	private int value;
+	private Account account;
+	private int id;
+	
+	
+	public MoneyGenerated(int value, Account account)
+	{
+		this.account = account;
+		this.value = value;
+	}
+
+
+	public int getValue() {
+		return value;
+	}
+
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+
+	public Account getAccount() {
+		return account;
+	}
+
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+	
+	
+}
